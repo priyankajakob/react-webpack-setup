cc7255d0e7061088f1d85ce0bf4891eeda44b5f6
Priyanka Jacob <priyanka.jacob93@gmail.com>
2023-05-20 11:34:35 +0530

    chore:add comments

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-// const sayHelloXTimes = (times) => new Array(times).fill(1).map((a,i)=>`Hello : ${i}`)
+// src/index.js
 
+//-------------------------------------
+// const newH2Element = document.createElement('h2');
+// newH2Element.innerHTML = 'This heading is added via code';
+// document.body.append(newH2Element);
+
+//-------------------------------------
+// Using ES6 Array.fill not supported in IE without it being transpiled.
+
+// const sayHelloXTimes = (times) => new Array(times).fill(1).map((a,i)=>`Hello : ${i}`)
 // const newH2Element = document.createElement('h2');
 // newH2Element.innerHTML = sayHelloXTimes(10).join(`<br/>`);
 // document.body.append(newH2Element);
 
 //--------------------------------------
-//Using React
+// Using React
+
 import { React } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import Welcome from './Welcome';
 
 const root = document.getElementById('root')
